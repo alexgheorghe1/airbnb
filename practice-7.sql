e14e7ee13a5f02d53fd965adbd2a6ad8970e478e
alexgheorghe1 <Alex.gheorghe@kellogg.northwestern.edu>
2022-01-16 19:09:59 +0000

    Practice Exercises

--- practice-7.sql
+++ practice-7.sql
@@ -17,3 +17,17 @@
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
 
+-- MY ANSWER (GET SAME RESULT):
+
+-- SELECT listings.property_type, COUNT(*), MAX(reviews.date_reviewed)
+-- FROM listings
+-- INNER JOIN reviews ON reviews.listing_id = listings.id
+-- GROUP BY property_type;
+
+-- HW ANSWER:
+
+SELECT listings.property_type, COUNT(*), MAX(reviews.date_reviewed)
+FROM reviews
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.property_type;
+
